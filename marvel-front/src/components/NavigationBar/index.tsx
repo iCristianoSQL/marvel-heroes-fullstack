ca16fca4fffef1f25f2620edcff614947cfca5fb
Cristiano dos Santos <cristianosql02@gmail.com>
2023-06-24 13:46:36 -0400

    feat: adicionando links na barra de navegação da header

--- marvel-front/src/components/NavigationBar/index.tsx
+++ marvel-front/src/components/NavigationBar/index.tsx
@@ -1,10 +1,17 @@
-import * as S from './styles';
-import Logo from '../../assets/images/logo.png';
+import * as S from "./styles";
+import Logo from "../../assets/images/logo.png";
 
 export const NavigationBar = () => {
-    return (
-        <S.Header>
-            <img src={Logo} alt="Logo" />
-        </S.Header>
-    )
-}
+  return (
+    <S.Header>
+      <img src={Logo} alt="Logo" />
+
+      <S.Nav>
+        <a href="#">Início</a>
+        <a href="">Informação</a>
+        <a href="">Heróis</a>
+        <a href="">Painel</a>
+      </S.Nav>
+    </S.Header>
+  );
+};
--- marvel-front/src/components/NavigationBar/styles.ts
+++ marvel-front/src/components/NavigationBar/styles.ts
@@ -1,11 +1,36 @@
 import { styled } from "styled-components";
 
 export const Header = styled.header`
-    background-color: white;
     height: 5rem;
     width: 100%;
+    padding: 0 2rem;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 
     img {
         height: 100%;
+    }
+`;
+
+export const Nav = styled.nav`
+    width: fit-content;
+    background-color: black;
+    height: 100%;
+
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    
+    a {
+        font-weight: 500;
+        font-size: 1.25rem;
+        transition: 0.2s;
+        font-family: 'Inter', sans-serif;
+
+        &:hover {
+            border-bottom: 2px solid white;
+        }
     }
 `;
