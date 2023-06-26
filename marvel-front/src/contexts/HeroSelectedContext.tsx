f2f86c7780a976005bbf4e05359255c73531c04e
Cristiano dos Santos <cristianosql02@gmail.com>
2023-06-26 18:40:38 -0400

    refactor: ajustando trativa de valor temporario

--- marvel-front/src/contexts/HeroSelectedContext.tsx
+++ marvel-front/src/contexts/HeroSelectedContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { ReactNode, createContext, useState } from "react";
 import { IHero } from "../utils/@types";
 import { heroes } from "../utils/heroes";
 
@@ -16,15 +16,15 @@ heroes.forEach((hero) => {
   ids.push(hero.id);
 });
 const randomIndex = Math.floor(Math.random() * ids.length);
-const heroFinded = heroes.find((hero) => hero.id === ids[randomIndex]);
+const heroFinded = heroes.find((hero) => hero.id === ids[randomIndex]) ?? {} as IHero;
 
-const HeroSelectedProvider = ({children}: any) => {
+const HeroSelectedProvider = ({children}: { children: ReactNode }) => {
   const [hero, setHero] = useState<IHero>({
-    id: heroFinded?.id ?? 4,
-    name: heroFinded?.name ?? "Cyclope",
-    banner: heroFinded?.banner ?? '',
-    image: heroFinded?.image ?? "https://terrigen-cdn-dev.marvel.com/content/prod/1x/002cyc_com_crd_01.jpg",
-    description: heroFinded?.description ?? "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam repudiandae ipsa laborum, non reiciendis laboriosam nobis iure modi ipsam officia voluptate ea amet fugit culpa sapiente, voluptatum et unde quaerat?",
+    id: heroFinded?.id,
+    name: heroFinded?.name,
+    banner: heroFinded?.banner,
+    image: heroFinded?.image,
+    description: heroFinded?.description,
   });
 
   const setHeroValue = (newValue: IHero) => {
