78291c8e09defb12c66fbaf5e538f0bba46637b2
Cristiano dos Santos <cristianosql02@gmail.com>
2023-06-26 19:45:54 -0400

    feat: criando controller de champions e retornando JSON

---
+++ marvel-back/app/controllers/champions_controller.rb
@@ -0,0 +1,45 @@
+class ChampionsController < ApplicationController
+    before_action :set_champion, only: [:show, :update, :destroy]
+  
+    def index
+      @champions = Champion.all
+      render json: @champions
+    end
+  
+    def show
+      render json: @champion
+    end
+  
+    def create
+      @champion = Champion.new(champion_params)
+  
+      if @champion.save
+        render json: @champion, status: :created
+      else
+        render json: @champion.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def update
+      if @champion.update(champion_params)
+        render json: @champion
+      else
+        render json: @champion.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def destroy
+      @champion.destroy
+      head :no_content
+    end
+  
+    private
+  
+    def set_champion
+      @champion = Champion.find(params[:id])
+    end
+  
+    def champion_params
+      params.require(:champion).permit(:name, :description, :team_id, :image)
+    end
+end  
