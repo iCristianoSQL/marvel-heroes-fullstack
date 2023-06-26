dfead4862b9709cb56e78f6f9a7f2d0efbf2d668
Cristiano dos Santos <cristianosql02@gmail.com>
2023-06-26 19:48:35 -0400

    feat: nova tabela no hsitorico do schema

--- marvel-back/db/schema.rb
+++ marvel-back/db/schema.rb
@@ -10,9 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_26_223119) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_26_233451) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "champions", force: :cascade do |t|
+    t.string "name", limit: 20, null: false
+    t.text "description", null: false
+    t.bigint "team_id"
+    t.string "image", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_champions_on_name", unique: true
+    t.index ["team_id"], name: "index_champions_on_team_id"
+  end
+
+  create_table "champions_skills", force: :cascade do |t|
+    t.bigint "champion_id", null: false
+    t.bigint "skill_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["champion_id"], name: "index_champions_skills_on_champion_id"
+    t.index ["skill_id"], name: "index_champions_skills_on_skill_id"
+  end
 
   create_table "skills", force: :cascade do |t|
     t.string "name", null: false
@@ -32,4 +52,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_26_223119) do
     t.index ["name"], name: "index_teams_on_name", unique: true
   end
 
+  add_foreign_key "champions", "teams"
+  add_foreign_key "champions_skills", "champions"
+  add_foreign_key "champions_skills", "skills"
 end
