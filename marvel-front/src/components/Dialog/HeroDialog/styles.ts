7f1bcca204df23b1a60960407561d13d3cc681c3
Cristiano dos Santos <cristianosql02@gmail.com>
2023-06-26 03:34:09 -0400

    style: estilizando o nome e a descrição

--- marvel-front/src/components/Dialog/HeroDialog/styles.ts
+++ marvel-front/src/components/Dialog/HeroDialog/styles.ts
@@ -1,7 +1,40 @@
 import { styled } from "styled-components";
 import { IHeroDialog } from "./types";
+import { colors } from "../../../utils/colors";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+
+    .image-relative-box {
+        position: relative;
+        
+        span {
+            bottom: 0;
+            position: absolute;
+            height: 100%;
+            width: 100%;
+            background-image: linear-gradient(to top, ${colors.primary["0.1"]} 30%, transparent 70%);
+            display: flex;
+            align-items: flex-end;
+
+            h1 {
+                color: ${colors.primary[1]} !important;
+                font-size: 3.5rem;
+            }
+        }
+    }
+
+    .description-box {
+        margin-top: 0.5rem;
+        strong {
+            font-size: 1.4rem;
+            border-bottom: 1px solid ${colors.primary[1]};
+        }
+        p {
+            margin-top: 0.5rem;
+            font-size: 1rem;
+        }
+    }
+`;
 
 export const Image = styled.div<IHeroDialog>`
   width: 100%;
