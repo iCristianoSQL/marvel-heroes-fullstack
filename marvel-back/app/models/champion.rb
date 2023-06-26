f533b1071336b54fb19a5d138162692b206ca6ed
Cristiano dos Santos <cristianosql02@gmail.com>
2023-06-26 19:46:49 -0400

    feat: criando a model de validação e apicando relações e validações a nivel de aplicação

---
+++ marvel-back/app/models/champion.rb
@@ -0,0 +1,7 @@
+class Champion < ApplicationRecord
+  belongs_to :team, optional: true
+  has_and_belongs_to_many :skills
+
+  validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
+  validates :description, presence: true, length: { maximum: 650 }
+end
