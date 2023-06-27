33d57de553e6be798cfc337a55a98c7bb9b32c8e
Cristiano dos Santos <cristianosql02@gmail.com>
2023-06-26 20:41:36 -0400

    refactor: v2 barra de navegação  entre a plataforma atualizada

--- marvel-front/src/components/NavigationBar/index.tsx
+++ marvel-front/src/components/NavigationBar/index.tsx
@@ -1,16 +1,18 @@
 import * as S from "./styles";
 import Logo from "../../assets/images/logo.png";
+import { useNavigate } from "react-router-dom";
 
 export const NavigationBar = () => {
+  const navigate = useNavigate()
   return (
     <S.Header>
       <img src={Logo} alt="Logo" />
 
       <S.Nav>
-        <a href="#">Início</a>
+        <a href="#" onClick={() => navigate('/')}>Início</a>
         <a href="#infos">Informação</a>
         <a href="#heroes">Heróis</a>
-        <a href="https://www.google.com.br" target="_blank" rel='noopener'>Painel</a>
+        <a onClick={() => navigate('/painel')} rel='noopener'>Painel</a>
       </S.Nav>
     </S.Header>
   );
