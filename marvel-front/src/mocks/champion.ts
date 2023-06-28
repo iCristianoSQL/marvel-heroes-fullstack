fa9af205b899e12296bff41835ce9354cac44097
Cristiano dos Santos <cristianosql02@gmail.com>
2023-06-27 20:57:07 -0400

    feat: adicionando mock de campeão inicial na aplicação (easter egg)

---
+++ marvel-front/src/mocks/champion.ts
@@ -0,0 +1,12 @@
+import { IChampion } from "../utils/@types";
+
+import AntManIMG from "../assets/images/default_hero/ant-man.jpg";
+import AntManGIF from "../assets/images/default_hero/ant-man.gif";
+
+export const initialChampion: IChampion = {
+    id: 1,
+    name: "Homem Formiga",
+    image: AntManIMG,
+    banner: AntManGIF,
+    description: "O Homem-Formiga, também conhecido como Scott Lang, é um herói da Marvel que possui a incrível capacidade de encolher e aumentar de tamanho à vontade. Com um traje especial e a ajuda de partículas subatômicas, ele tem o poder de controlar a sua massa e a força proporcional a ela. Scott Lang utiliza essas habilidades extraordinárias para combater o crime e proteger o mundo de ameaças. Com seu carisma e inteligência, o Homem-Formiga traz uma abordagem única aos desafios, tornando-se um herói capaz de enfrentar perigos tanto no tamanho de um inseto quanto de um gigante. Sua determinação e habilidades especiais o tornam uma força imprevisível e essencial na luta pela justiça.",
+  };
