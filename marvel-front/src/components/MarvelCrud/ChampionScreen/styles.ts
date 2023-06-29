ddcf9655f328f92fe678ab4e604fa6ff3031d89d
Cristiano dos Santos <cristianosql02@gmail.com>
2023-06-29 00:43:00 -0400

    style: estilizando formulario

--- marvel-front/src/components/MarvelCrud/ChampionScreen/styles.ts
+++ marvel-front/src/components/MarvelCrud/ChampionScreen/styles.ts
@@ -2,6 +2,8 @@ import { styled } from "styled-components";
 import { colors } from "../../../utils/colors";
 
 export const Container = styled.div`
+  padding: 0 4rem;
+
   h1 {
     text-align: center;
   }
@@ -9,73 +11,109 @@ export const Container = styled.div`
     display: flex;
   }
 
-  .images-box {
+  .champion-image {
+    width: 13rem;
+    height: 22.985rem;
+    border-width: 1px;
+    border-style: solid;
+    border-image: linear-gradient(to bottom, blue, red) 1;
     display: flex;
-    .profile-image-box {
-        width: fit-content;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        position: relative;
+    flex-direction: column;
+    position: relative;
+    overflow: hidden;
 
-        .champion-image {
-            width: 13rem;
-            height: 22.985rem;
-            border-radius: 10px;
-            border-width: 1px;
-            border-style: solid;
-            border-image: linear-gradient(to bottom, blue, red) 1;
-            display: flex;
-
-            img {
-                width: 100%;
-                height: 100%;
-            }
-            
-            svg {
-                margin: auto;
-                font-size: 3.5rem;
-                fill: ${colors.primary["1.1"]};
-            }
-        }
-
-        span {
-            position: absolute;
-            left: 5px;
-            bottom: 35px;
-            width: 100%;
-            word-wrap: break-word;
-
-            h1 {
-                color: ${colors.primary[1]} !important;
-                font-size: 1.5rem;
-            }
-        }
+    img {
+      width: 100%;
+      height: 100%;
     }
 
-    .banner-image-box {
-      width: 5rem;
+    svg {
+      margin: auto;
+      font-size: 3.5rem;
+      fill: ${colors.primary["1.1"]};
+      align-self: center;
+    }
 
-      img {
-        width: 100%;
+    span {
+      position: absolute;
+      left: 5px;
+      bottom: 35px;
+      width: fit-content;
+      bottom: 0;
+      word-break: break-word;
+
+      h1 {
+        color: ${colors.primary[1]} !important;
+        font-size: 1.5rem;
+        text-align: initial;
       }
+    }
+  }
+
+  .banner-image {
+    width: 100%;
+    height: 13rem;
+    border-width: 1px;
+    border-style: solid;
+    border-image: linear-gradient(to bottom, blue, red) 1;
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    overflow: hidden;
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
+
+    svg {
+      margin: auto;
+      font-size: 3.5rem;
+      fill: ${colors.primary["1.1"]};
+      align-self: center;
     }
   }
 `;
 
 export const FileInput = styled.div`
-  input[type='file'] {
+  input[type="file"] {
     display: none;
   }
+`;
 
-  .custom-file-upload {
-    display: inline-block;
-    padding: 8px 16px;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    background-color: #f0f0f0;
-    color: #555;
-    cursor: pointer;
+export const Form = styled.form`
+  display: flex;
+  justify-content: center;
+  position: relative;
+
+  width: 100%;
+  gap: 1rem;
+
+  .first-column-form {
+      width: 30rem;
+      height: 23rem;
+
+      input {
+        width: 100%;
+      }
+  }
+
+  .second-column-form {
+    height: 100%;
+    width: 22.985rem;
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+
+    textarea {
+      height: 8rem;
+      width: 100%;
+    }
+  }
+
+  button {
+    position: absolute;
+    right: 0;
+    bottom: 0;
   }
 `;
