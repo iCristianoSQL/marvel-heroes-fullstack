2645b7189c891bf3d07f46fb7a61fd60bfd8def1
Cristiano dos Santos <cristianosql02@gmail.com>
2023-06-29 00:39:51 -0400

    feat: listagem de habilidades + tipagens

--- marvel-front/src/services/marvel.ts
+++ marvel-front/src/services/marvel.ts
@@ -1,13 +1,21 @@
 import { AxiosResponse } from "axios";
-import { IGetChampionResponse } from "../utils/@types";
+import { IGetChampionsResponse, IGetSkillsResponse } from "../utils/@types";
 import { api } from "./api";
 import { useQuery, UseQueryResult } from "react-query";
 
 export const MarvelServices = {
-  useGetChampions: (): UseQueryResult<IGetChampionResponse> => {
-    return useQuery<IGetChampionResponse>(["lista-de-campeoes"], async () => {
-      const response: AxiosResponse<IGetChampionResponse> =
-        await api.get<IGetChampionResponse>("champions");
+  useGetChampions: (): UseQueryResult<IGetChampionsResponse> => {
+    return useQuery<IGetChampionsResponse>(["lista-de-campeoes"], async () => {
+      const response: AxiosResponse<IGetChampionsResponse> =
+        await api.get<IGetChampionsResponse>("champions");
+      return response.data;
+    });
+  },
+
+  useGetSkills: (): UseQueryResult<IGetSkillsResponse> => {
+    return useQuery<IGetSkillsResponse>(["lista-de-habilidades"], async () => {
+      const response: AxiosResponse<IGetSkillsResponse> =
+        await api.get<IGetSkillsResponse>("skills");
       return response.data;
     });
   },
--- marvel-front/src/utils/@types.ts
+++ marvel-front/src/utils/@types.ts
@@ -6,7 +6,54 @@ export interface IChampion {
     description: string;
   }
 
-  export interface IGetChampionResponse {
+  export interface IGetChampionsResponse {
     message: string;
     champions: IChampion[];
   }
+
+  export interface ISkills {
+    id: number;
+    name: string;
+    description: string;
+    color: string;
+  }
+
+  export interface IGetSkillsResponse {
+    message: string;
+    skills: ISkills[];
+  }
+
+  export interface IImgbbData {
+    id: string;
+    title: string;
+    url_viewer: string;
+    url: string;
+    display_url: string;
+    width: number;
+    height: number;
+    size: number;
+    time: number;
+    expiration: number;
+    image: {
+      filename: string;
+      name: string;
+      mime: string;
+      extension: string;
+      url: string;
+    };
+    thumb: {
+      filename: string;
+      name: string;
+      mime: string;
+      extension: string;
+      url: string;
+    };
+    delete_url: string;
+  }
+
+  export interface IImgbbResponse {
+    data: IImgbbData;
+    success: boolean;
+    status: number;
+  }
+  
