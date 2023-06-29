7d755c78a4deca16901ce4568351ef64539c4de3
Cristiano dos Santos <cristianosql02@gmail.com>
2023-06-29 00:37:47 -0400

    feat: populan do o seed com o json criado, estruturando o laço

--- marvel-back/db/seeds.rb
+++ marvel-back/db/seeds.rb
@@ -3,6 +3,9 @@ require 'json'
 json_champions = File.read(Rails.root.join('db', 'champions.json'))
 champions = JSON.parse(json_champions)
 
+json_skills = File.read(Rails.root.join('db', 'skills.json'))
+skills = JSON.parse(json_skills)
+
 champions.each do |champion|
   begin
     Champion.create!(
@@ -14,5 +17,18 @@ champions.each do |champion|
     puts "Campeão #{champion['name']} adicionado com sucesso!"
   rescue ActiveRecord::RecordInvalid => e
     puts "#{champion['name']}: #{e.message}"
+  end
+end
+
+skills.each do |skill|
+  begin
+    Skill.create!(
+      name: skill['name'],
+      description: skill['description'],
+      color: skill['color'],
+    )
+    puts "Habilidade #{skill['name']} adicionada com sucesso!"
+  rescue ActiveRecord::RecordInvalid => e
+    puts "#{skill['name']}: #{e.message}"
   end
 end
