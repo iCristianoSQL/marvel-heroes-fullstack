0abc86cf480a1ae194090dffc62fc10819023457
Cristiano dos Santos <cristianosql02@gmail.com>
2023-06-29 00:42:50 -0400

    feat: construindo logica para submit dos dados do formulario e ajustando a esrutura do mesmo!

--- marvel-front/src/components/MarvelCrud/ChampionScreen/index.tsx
+++ marvel-front/src/components/MarvelCrud/ChampionScreen/index.tsx
@@ -1,51 +1,83 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import * as S from "./styles";
 import { AiOutlineFileImage } from "react-icons/ai";
 import TextInput from "../../TextInput";
 import TextArea from "../../TextArea";
+import { useMutation } from "react-query";
+import { uploadImage } from "../../../services/imgBB";
+import { Button, SkillList } from "../..";
+import { MarvelServices } from "../../../services/marvel";
+import { ISkills } from "../../../utils/@types";
 
 const schema = z.object({
   name: z.string().max(20).nonempty(),
   description: z.string().max(2000).nonempty(),
   team: z.number().optional(),
-  image: z.string().nonempty(),
-  banner: z.string().nonempty(),
+  image: z.string().optional(),
+  banner: z.string().optional(),
 });
 
 type ChampionSchema = z.infer<typeof schema>;
 
 export const ChampionScreen = () => {
-  const [image1080x1920, setImage1080x1920] = useState<string | null>(null);
-  const [image1920x1080, setImage1920x1080] = useState<string | null>(null);
-  const [heroName, setHeroName] = useState("Wolverine");
+  const [championImage, setChampionImage] = useState<string | null>(null);
+  const [bannerImage, setBannerImage] = useState<string | null>(null);
+  const fileInputRef = useRef<File | null>(null);
+  const [selectedSkill, setSelectedSkill] = useState<ISkills>({} as ISkills);
+  console.log(selectedSkill)
+
+  const handleSkillSelect = (skill: ISkills) => {
+    setSelectedSkill(skill);
+  };
+
+  const uploadImageMutation = useMutation(uploadImage);
 
   const {
     register,
     handleSubmit,
+    watch,
     formState: { errors },
   } = useForm<ChampionSchema>();
 
-  const onSubmit = (data: ChampionSchema) => {
-    console.log(data);
-  };
+  const championName = watch("name");
 
-  const handleImage1080x1920Change = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    const file = event.target.files && event.target.files[0];
-    if (file) {
-      setImage1080x1920(URL.createObjectURL(file));
+  const onSubmit = async (data: ChampionSchema) => {
+    try {
+      if (fileInputRef.current) {
+        const result = await uploadImageMutation.mutateAsync(
+          fileInputRef.current
+        );
+
+        if (result && result.data && result.data.display_url) {
+          const updatedData: ChampionSchema = {
+            ...data,
+            banner: result.data.display_url,
+          };
+        } else {
+          console.log("Ainda não deu certo");
+        }
+      }
+    } catch (error) {
+      console.error(error);
     }
   };
 
-  const handleImage1920x1080Change = (
+  const handleChampionImage = async (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
     const file = event.target.files && event.target.files[0];
     if (file) {
-      setImage1920x1080(URL.createObjectURL(file));
+      setChampionImage(URL.createObjectURL(file));
+      fileInputRef.current = file;
+    }
+  };
+
+  const handleBannerImage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files && event.target.files[0];
+    if (file) {
+      setBannerImage(URL.createObjectURL(file));
     }
   };
 
@@ -53,86 +85,78 @@ export const ChampionScreen = () => {
     <S.Container>
       <h1>ChampionScreen</h1>
       <div className="register-champion-box">
-        <div className="images-box">
-          <div className="profile-image-box">
-            <div className="champion-image">
-              {image1080x1920 && (
-                <>
-                  <img src={image1080x1920} alt="Image 1080x1920" />
-                  <span>
-                    <h1>{heroName}</h1>
-                  </span>
-                </>
-              )}
-              {!image1080x1920 && <AiOutlineFileImage />}
-            </div>
+        <S.Form onSubmit={handleSubmit(onSubmit)}>
+          <S.FileInput>
+            <input
+              type="file"
+              id="championImage"
+              onChange={handleChampionImage}
+            />
+            <label htmlFor="championImage" className="custom-file-upload">
+              <div className="champion-image">
+                {championImage && (
+                  <>
+                    <img src={championImage} alt="Champion Image" />
+                    <span>
+                      <h1>{championName}</h1>
+                    </span>
+                  </>
+                )}
+                {!championImage && <AiOutlineFileImage />}
+              </div>
+            </label>
+          </S.FileInput>
+          <div className="first-column-form">
+            <TextInput
+              label="Nome do Herói:"
+              type="text"
+              id="name"
+              register={register("name", { required: true })}
+            />
+            {errors.name && (
+              <span className="error">O nome é obrigatório.</span>
+            )}
+
+            <TextInput
+              label="Equipe:"
+              type="number"
+              id="team"
+              register={register("team")}
+            />
+
+            <SkillList onSelect={handleSkillSelect} width="100%" height="16rem" className="skills-list"/>
+          </div>
+
+          <div className="second-column-form">
+            <TextArea
+              label="Descrição:"
+              id="description"
+              register={register("description", { required: true })}
+            />
+            {errors.description && (
+              <span className="error">A descrição é obrigatória.</span>
+            )}
+
             <S.FileInput>
-              <label className="custom-file-upload">
-                Selecionar arquivo
-                <input
-                  type="file"
-                  id="image1080x1920"
-                  onChange={handleImage1080x1920Change}
-                />
+              <input
+                type="file"
+                id="bannerImage"
+                onChange={handleBannerImage}
+              />
+              <label htmlFor="bannerImage" className="custom-file-upload">
+                <div className="banner-image">
+                  {bannerImage && (
+                      <img src={bannerImage} alt="Banner" />
+                  )}
+                  {!bannerImage && <AiOutlineFileImage />}
+                </div>
               </label>
             </S.FileInput>
           </div>
-        </div>
 
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <TextInput
-            label="Nome do Herói:"
-            type="text"
-            id="name"
-            register={register("name", { required: true })}
-          />
-          {errors.name && <span className="error">O nome é obrigatório.</span>}
-
-          <TextArea
-            label="Descrição:"
-            id="description"
-            register={register("description", { required: true })}
-          />
-          {errors.description && (
-            <span className="error">A descrição é obrigatória.</span>
-          )}
-
-          <TextInput
-            label="Equipe:"
-            type="number"
-            id="team"
-            register={register("team")}
-          />
-
-          <TextInput
-            label="Imagem:"
-            type="text"
-            id="image"
-            register={register("image", { required: true })}
-          />
-          {errors.image && (
-            <span className="error">A imagem é obrigatória.</span>
-          )}
-
-          <TextInput
-            label="Banner:"
-            type="text"
-            id="banner"
-            register={register("banner", { required: true })}
-          />
-          {errors.banner && (
-            <span className="error">O banner é obrigatório.</span>
-          )}
-
-          <button type="submit">Enviar</button>
-        </form>
+          <Button type="submit">Enviar</Button>
+        </S.Form>
       </div>
     </S.Container>
   );
 };
-
-/* <div className='banner-image-box'>
-            {image1920x1080 && <img src={image1920x1080} alt="Image 1920x1080"/>}
-            <label htmlFor="image1920x1080">Image 1920x1080:</label>
-            <input type="file" id="image1920x1080" onChange={handleImage1920x1080Change} />
-        </div> */
