079aa701feaccc39e357c9c5da55538e4246ebbb
Cristiano dos Santos <cristianosql02@gmail.com>
2023-06-30 02:51:49 -0400

    feat: criando, estilizando e passando as tipagens do componente de select do sistema!

---
+++ marvel-front/src/components/SelectInput/styles.ts
@@ -0,0 +1,43 @@
+import styled, { css } from "styled-components";
+import { colors } from "../../utils/colors";
+import { IStyledSelectInput } from "./types";
+
+export const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+
+  font-weight: 600;
+  font-size: 1rem;
+  line-height: 1.1rem;
+  color: ${colors.danger[0]};
+`;
+
+export const SelectInput = styled.select<IStyledSelectInput>`
+  outline: none;
+  box-shadow: none;
+  border-radius: 5px;
+  border: 1px solid ${colors.primary[0.1]};
+  background: ${colors.primary[1]};
+  color: ${colors.primary[0]};
+  font-weight: 500;
+  font-size: 1rem;
+  padding: 0 0.5rem;
+
+  &::placeholder {
+    font-weight: 500;
+    opacity: 0.6;
+  }
+
+  ${({ height, width }) =>
+    css`
+      height: ${height ?? "35px"};
+      width: ${width ?? "250px"};
+    `}
+
+    option {
+    background: ${colors.primary[1]};
+    color: ${colors.primary[0]};
+    font-weight: 500;
+  }
+`;
