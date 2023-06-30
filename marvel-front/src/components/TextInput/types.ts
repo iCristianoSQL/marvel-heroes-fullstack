808f52fdc92f34a45387acefef672c856275db13
Cristiano dos Santos <cristianosql02@gmail.com>
2023-06-29 22:30:03 -0400

    refactor: adicionando opicionalidade no label

--- marvel-front/src/components/TextInput/types.ts
+++ marvel-front/src/components/TextInput/types.ts
@@ -3,6 +3,6 @@ import { InputHTMLAttributes } from "react";
 export interface IStyledTextInput extends InputHTMLAttributes<HTMLInputElement> {
     height?: string;
     width?: string;
-    label: string;
+    label?: string;
     register: any;
 }
