f323fb514fa30dc9498b84e40dbd1dca827d5f22
Cristiano dos Santos <cristianosql02@gmail.com>
2023-07-03 18:09:33 -0400

    style: ajustando estilos

--- marvel-front/src/components/MarvelCrud/ChampionScreen/styles.ts
+++ marvel-front/src/components/MarvelCrud/ChampionScreen/styles.ts
@@ -3,6 +3,7 @@ import { colors } from "../../../utils/colors";
 
 export const Container = styled.div`
   padding: 0 4rem;
+  margin-top: 1rem;
 
   h1 {
     text-align: center;
@@ -30,21 +31,6 @@ export const Container = styled.div`
       font-size: 3.5rem;
       fill: ${colors.primary["1.1"]};
       align-self: center;
-    }
-
-    span {
-      position: absolute;
-      left: 5px;
-      bottom: 35px;
-      width: fit-content;
-      bottom: 0;
-      word-break: break-word;
-
-      h1 {
-        color: ${colors.primary[1]} !important;
-        font-size: 1.5rem;
-        text-align: initial;
-      }
     }
   }
 
